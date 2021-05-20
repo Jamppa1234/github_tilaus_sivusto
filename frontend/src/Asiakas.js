81ed2c6d84f87a74a28c3c3d0bc487fb770bc58c
Jamppa1234 <finjarnom@gmail.com>
2021-05-20 15:33:30 +0300

    lisätty tausta pal7

--- frontend/src/Asiakas.js
+++ frontend/src/Asiakas.js
@@ -4,4 +4,20 @@ function Asiakas(props) {
     return <h1>{props.title}  </h1>
 }
 
+// get asiakkaat
+app.get('/getasiakkaat', (req, res) => {
+    let sql = 'SELECT * FROM asiakkaat'
+    let query = db.query(sql, (err, results) => {
+        if (err) {
+            throw err;
+        }
+        console.log(results);
+        res.send('Asiakkaat details fetched');
+    })
+})
+
+app.listen('4000', () => {
+    console.log('Server started on port 4000')
+})
+
 export default Asiakas;
